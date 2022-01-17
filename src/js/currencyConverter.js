751bb50d7bc09c80dd0592296ee70d765c48c111
Phil Curran <pecurran@hotmail.com>
2022-01-17 12:03:22 -0800

    added static method for api call that gets supported currencies

--- src/js/currencyConverter.js
+++ src/js/currencyConverter.js
@@ -1,4 +1,22 @@
 export default class CurrencyExchange {
+  static populateCurrencySelection() {
+    fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/codes`)
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then((response) => {
+        response.supported_codes.forEach((code) => {
+          console.log(`<option value="${code[0]}">${code[1]}</option>`);
+        });
+      })
+      .catch(function (error) {
+        return error;
+      });
+  }
+
   static convertCurrency(convertFrom, convertTo) {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${convertFrom}/${convertTo}
     `)
@@ -12,15 +30,4 @@ export default class CurrencyExchange {
         return error;
       });
   }
-  static getCurrency(currency) {}
-  return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
-    .then(function (response) {
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      return response.json();
-    })
-    .catch(function (error) {
-      return error;
-    });
 }
